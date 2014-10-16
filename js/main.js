3c3cba32e895950781cf6fd049c789adb359c14f
Sergey Vinogradov <pettsonchello@gmail.com>
2014-10-16 19:13:30 +0400

    Работа над слайдером

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ $(document).ready(function() {
         until: '+2h'
     });
 
+    $('.b-catalog__filter__item').click(function(e) {
+        $('.b-catalog__filter__item').removeClass('active');
+
+        $(this).addClass('active');
+
+        e.preventDefault();
+    });
+
+    $('.b-product-slider__slides').bxSlider();
 });
